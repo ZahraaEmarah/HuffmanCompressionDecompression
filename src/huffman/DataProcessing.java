eee875085d449037537f74357e81dd83a8a85156
Jarvis <emarahzahraa@gmail.com>
2019-11-30 14:42:21 +0200

    ..

---
+++ src/huffman/DataProcessing.java
@@ -0,0 +1,66 @@
+package huffman;
+import java.io.File;  
+import java.io.FileNotFoundException;  
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class DataProcessing {
+
+	public static void main(String[] args) {
+		String data = "";
+		
+	    try {
+	      File file = new File("original.txt");
+	      Scanner myReader = new Scanner(file);
+	      while (myReader.hasNext()) {
+	         data = data + myReader.next();
+	      }
+	      myReader.close();
+	    } catch (FileNotFoundException e) {
+	      System.out.println("File Not Found");
+	    }
+	    
+	    char[] charArray = data.toCharArray();
+		ArrayList<Character> chars = new ArrayList<Character>();
+		for (int i = 0; i < charArray.length; i++) {
+			if (!(chars.contains(charArray[i]))) {
+				chars.add(charArray[i]);
+			}
+		}
+		System.out.println(chars);
+		
+		int[] countOfChar = new int[chars.size()];
+		for (int x = 0; x < countOfChar.length; x++) {
+			countOfChar[x] = 0;
+		}
+
+		for (int i = 0; i < chars.size(); i++) {
+			char checker = chars.get(i);
+			for (int x = 0; x < charArray.length; x++) {
+				if (checker == charArray[x]) {
+					countOfChar[i]++;
+				}
+			}
+		}
+		
+		for (int i = 0; i < countOfChar.length - 1; i++) {
+			for (int j = 0; j < countOfChar.length - 1; j++) {
+				if (countOfChar[j] > countOfChar[j + 1]) {
+					int temp = countOfChar[j];
+					countOfChar[j] = countOfChar[j + 1];
+					countOfChar[j + 1] = temp;
+
+					char tempChar = chars.get(j);
+					chars.set(j, chars.get(j + 1));
+					chars.set(j + 1, tempChar);
+				}
+			}
+		}
+		
+		for (int x = 0; x < countOfChar.length; x++) {
+			System.out.println(chars.get(x) + " - " + countOfChar[x]);
+		}
+		 
+	  }
+	
+}
